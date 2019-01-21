d378bac59b29d96214dc60c18ea712c128659593
Geoff St. Pierre <serundeputy@gmail.com>
2019-01-21 14:34:49 -0500

    Account for tiered paths.

--- components/AppCurrentLabel.vue
+++ components/AppCurrentLabel.vue
@@ -7,18 +7,23 @@
 export default {
   data() {
     let path = this.$route.path
-    if (path.length > 1) {
-      path = path.substring(1)
+    path = path.split('/')
+    if (path.length == 3) {
+      path = path[2]
+    } else if (path.length == 2) {
+      path = path[1]
     } else {
       path = 'home'
     }
+    console.log(path)
     const myRoutes = {
       home: '*latest writings',
       about: '*about',
       archives: '*archives',
       contributions: '*contributions',
       writings: '*writings',
-      events: '*events'
+      events: '*events',
+      past: '*past events'
     }
 
     return {
