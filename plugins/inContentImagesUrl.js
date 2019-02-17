d6d314ca76cd5545a209507fdb2c6cef10e34e29
Geoff St. Pierre <serundeputy@gmail.com>
2019-02-17 18:57:55 -0500

    Update inline image getter for new api url.

--- plugins/inContentImagesUrl.js
+++ plugins/inContentImagesUrl.js
@@ -3,7 +3,7 @@ export default {
    * Swap out inline content images with full URL.
    */
   imgUrl(content) {
-    let baseUrl = process.env.API_URL || 'https://serundeputy.io'
+    let baseUrl = process.env.API_URL || 'https://api.serundeputy.io'
     let contentWithImages = content.replace(/\/files\//g, baseUrl + '/files/')
 
     return contentWithImages
